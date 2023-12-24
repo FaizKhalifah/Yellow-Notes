b20eaf6646d7a52c3081aa01fe60e6152b9c8eed
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2023-12-24 22:26:17 +0700

    Fungsi menambah, setengah berhasil

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const ul = document.querySelector("#listTugas");
+
 const formTambah=document.forms['formTambah'];
 formTambah.addEventListener('submit',function(e){
     e.preventDefault();
@@ -6,5 +7,25 @@ formTambah.addEventListener('submit',function(e){
     const tanggal = document.querySelector("#tanggal").value;
     console.log(masukanTugas);
     console.log(tanggal);
+
+    const listTugas = document.createElement('li');
+    const namaTugas=document.createElement('span');
+    const deadline=document.createElement('span');
+    const checkStatus = document.createElement('span');
+    const labelStatus = document.createElement('label');
+    const status = document.createElement('input');
+    status.setAttribute('type','checkbox')
+
+
+    //Menambah elemen pada span status
+    checkStatus.appendChild(labelStatus);
+    checkStatus.appendChild(status);
+
+    //Menambah elemen span pada li
+    listTugas.appendChild(namaTugas);
+    listTugas.appendChild(deadline);
+
+    //Menambah li pada ul
+    ul.appendChild(listTugas);
 })
 
