625ec4c6a17c3d08e4e6150118589335b666e597
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2023-12-24 21:54:14 +0700

    Menambahkan variabel untuk menyimpan nama tugas dan tanggal pada script

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const ul = document.querySelector("#listTugas");
+const formTambah=document.forms['formTambah'];
+formTambah.addEventListener('submit',function(e){
+    e.preventDefault();
+    const masukanTugas = document.querySelector("#masukan").value;
+    const tanggal = document.querySelector("#tanggal").value;
+    console.log(masukanTugas);
+    console.log(tanggal);
+})
 
